26a99e32eeeeba0675b85a12e73f086406a3f3c5
rodriguesy1997-collab <rodriguesy1997@outlook.com>
2026-08-25 23:41:32 -0300

    Delete serralheria-bomfim directory

--- serralheria-bomfim/src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-type ClassValue = string | false | null | undefined;
-
-/** Combina classes condicionalmente, ignorando valores falsy. */
-export function cn(...classes: ClassValue[]): string {
-  return classes.filter(Boolean).join(" ");
-}
